e3548dc4b87bfb851f848c34a1bca9b72b7b7b5e
HKT12 <ketanom2001@gmail.com>
2023-02-12 11:25:59 +0530

    workingA&R

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def admin_login(request:Request, email:str = Form(...), password:str=Form(...)):
     cursor.execute('select * from users where email =? and password=?', (email,password,))
     result = cursor.fetchone()
     if not result:
-        return templates.TemplateResponse("/login2.html", {"request": request, "msg": "Invalid Username or Password"})
+        return templates.TemplateResponse("/login2.html", {"request": request, "msg": "Invalid Email or Password"})
     else:
         request.session.setdefault("isLogin", True)
         request.session.setdefault('email', email)
@@ -63,7 +63,7 @@ def teacher_login(request:Request, email:str = Form(...), password:str=Form(...)
     cursor.execute('select * from addteachers where email =? and password=?', (email,password,))
     result = cursor.fetchone()
     if not result:
-        return templates.TemplateResponse("/login2.html", {"request": request, "msg": "Invalid Username or Password"})
+        return templates.TemplateResponse("/login2.html", {"request": request, "msg": "Invalid Email or Password"})
     else:
         request.session.setdefault("isLogin", True)
         request.session["email"] = email
@@ -71,7 +71,7 @@ def teacher_login(request:Request, email:str = Form(...), password:str=Form(...)
         return RedirectResponse("/Tdashboard", status_code=status.HTTP_302_FOUND)
 
 @app.get("/Tdashboard", response_class=HTMLResponse)
-def create(request:Request):
+def Tdashboard(request:Request):
     isLogin = request.session.get('isLogin')
     role = request.session.get('role')
     email = request.session.get('email')
@@ -99,13 +99,13 @@ def create(request:Request):
         visitors_business = cursor.fetchone()
         
         # Fetch the video records for the specified course
-        cursor.execute("SELECT * FROM register1 where whom_to_meet=?", (department_name,))
+        cursor.execute("SELECT * FROM register1 where whom_to_meet=? order by id DESC", (department_name,))
         orders = cursor.fetchall()
         
-        cursor.execute("SELECT * FROM register1 where whom_to_meet=? and purpose ='Personal'"  , (department_name,))
+        cursor.execute("SELECT * FROM register1 where whom_to_meet=? and purpose ='Personal'  order by id DESC "  , (department_name,))
         personal_visitors = cursor.fetchall()
         
-        cursor.execute("SELECT * FROM register1 where whom_to_meet=? and purpose ='Business'"  , (department_name,))
+        cursor.execute("SELECT * FROM register1 where whom_to_meet=? and purpose ='Business'  order by id DESC"  , (department_name,))
         business_visitors = cursor.fetchall()
         
        
@@ -116,7 +116,7 @@ def create(request:Request):
 
 
 @app.get("/FFdashboard", response_class=HTMLResponse)
-def create(request:Request):
+def FFdashboard(request:Request):
     isLogin = request.session.get('isLogin')
     role = request.session.get('role')
     email = request.session.get('email')
@@ -155,7 +155,7 @@ def create(request:Request):
 @app.post("/authorize", response_class=HTMLResponse)
 def authorize(request:Request, statuses:str=Query(None),id:int=Query(None)):    
     db.executeQueryWithParams("UPDATE register1 set status = ? where id =? ",[statuses,id])
-    return RedirectResponse('/FFdashboard', status_code=status.HTTP_302_FOUND)
+    return RedirectResponse('/Tdashboard', status_code=status.HTTP_302_FOUND)
 
 
 
@@ -188,7 +188,8 @@ def create_post(request:Request, name:str = Form(...),department:str = Form(...)
     data = {"name":name, "department":department, "contact_number":contact_number, "date_of_birth":date_of_birth, "email":email,"teachers_id":teachers_id, "password":password, "photo":file_path}
     if(db.insert("addteachers",data=data)):
         return templates.TemplateResponse("Fregister.html",{"request":request,"msg":"Account created successfully!!"})
-    return templates.TemplateResponse("Fregister.html",{"request":request,"msg":"Unable to create account"})
+    else:
+        return templates.TemplateResponse("Fregister.html",{"request":request,"msg":"Unable to create account"})
     
     
 @app.get("/login1", response_class=HTMLResponse)
@@ -211,10 +212,11 @@ def create(request:Request):
     cursor.execute("SELECT COUNT(*) FROM register1 where purpose ='Business'")
     visitors_business = cursor.fetchone()
     
-    teacher_meeting = db.executeQuery("select * from register1")
-    teacher_meeting_personal = db.executeQueryWithParams("select * from register1 where purpose =?", ["Personal"])
-    teacher_meeting_business = db.executeQueryWithParams("select * from register1 where purpose =?", ["Business"])
-    return templates.TemplateResponse("others.html",{"request":request, "teacher_meeting":teacher_meeting, "teacher_meeting_personal":teacher_meeting_personal, "teacher_meeting_business":teacher_meeting_business,  "total_visitors":total_visitors[0], "visitors_personal": visitors_personal[0], "visitors_business":visitors_business[0]})
+    teacher_meeting = db.executeQuery("select * from register1 order by id DESC")
+    teacher_meeting_personal = db.executeQueryWithParams("select * from register1 where purpose =? order by id DESC", ["Personal"])
+    teacher_meeting_business = db.executeQueryWithParams("select * from register1 where purpose =? order by id DESC", ["Business"])
+    orders = db.executeQuery("select * from register1 order by id DESC")
+    return templates.TemplateResponse("others.html",{"request":request,"orders":orders, "teacher_meeting":teacher_meeting, "teacher_meeting_personal":teacher_meeting_personal, "teacher_meeting_business":teacher_meeting_business,  "total_visitors":total_visitors[0], "visitors_personal": visitors_personal[0], "visitors_business":visitors_business[0]})
 
 @app.get("/login2", response_class=HTMLResponse)
 def create(request:Request):
